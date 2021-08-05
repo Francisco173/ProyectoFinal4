525124c232b50ea5ebc9221e7cb889f3ef8632e0
Francisco173 <francisconissero@gmail.com>
2021-08-05 14:38:24 -0300

    Final4

---
+++ codigo.js
@@ -0,0 +1,450 @@
+var NumberStepper = {
+    init: function () {
+      this.domElems();
+      this.events();
+    },
+    domElems: function () {
+      this.plusBtn = document.getElementById('plus');
+      this.minusBtn = document.getElementById('minus');
+      this.input = document.getElementById('input');
+    },
+    events: function () {
+      this.minusBtn.addEventListener('click', this.decrement.bind(this));
+      this.plusBtn.addEventListener('click', this.increment.bind(this));
+    },
+    decrement: function () {
+      var value = this.input.value;
+      value--;
+      this.input.value = value;  
+    },
+    increment: function () {
+      var value = this.input.value;
+      value++;
+      this.input.value = value;  
+    }
+  };
+  
+  NumberStepper.init();
+
+
+  var NumberStepper = {
+    init: function () {
+      this.domElems();
+      this.events();
+    },
+    domElems: function () {
+      this.plusBtn = document.getElementById('plus2');
+      this.minusBtn = document.getElementById('minus2');
+      this.input = document.getElementById('input2');
+    },
+    events: function () {
+      this.minusBtn.addEventListener('click', this.decrement.bind(this));
+      this.plusBtn.addEventListener('click', this.increment.bind(this));
+    },
+    decrement: function () {
+      var value = this.input.value;
+      value--;
+      this.input.value = value;  
+    },
+    increment: function () {
+      var value = this.input.value;
+      value++;
+      this.input.value = value;  
+    }
+  };
+  
+  NumberStepper.init();
+
+
+  var NumberStepper = {
+    init: function () {
+      this.domElems();
+      this.events();
+    },
+    domElems: function () {
+      this.plusBtn = document.getElementById('plus3');
+      this.minusBtn = document.getElementById('minus3');
+      this.input = document.getElementById('input3');
+    },
+    events: function () {
+      this.minusBtn.addEventListener('click', this.decrement.bind(this));
+      this.plusBtn.addEventListener('click', this.increment.bind(this));
+    },
+    decrement: function () {
+      var value = this.input.value;
+      value--;
+      this.input.value = value;  
+    },
+    increment: function () {
+      var value = this.input.value;
+      value++;
+      this.input.value = value;  
+    }
+  };
+  
+  NumberStepper.init();
+
+
+  var NumberStepper = {
+    init: function () {
+      this.domElems();
+      this.events();
+    },
+    domElems: function () {
+      this.plusBtn = document.getElementById('plus4');
+      this.minusBtn = document.getElementById('minus4');
+      this.input = document.getElementById('input4');
+    },
+    events: function () {
+      this.minusBtn.addEventListener('click', this.decrement.bind(this));
+      this.plusBtn.addEventListener('click', this.increment.bind(this));
+    },
+    decrement: function () {
+      var value = this.input.value;
+      value--;
+      this.input.value = value;  
+    },
+    increment: function () {
+      var value = this.input.value;
+      value++;
+      this.input.value = value;  
+    }
+  };
+  
+  NumberStepper.init();
+
+  var NumberStepper = {
+    init: function () {
+      this.domElems();
+      this.events();
+    },
+    domElems: function () {
+      this.plusBtn = document.getElementById('plus5');
+      this.minusBtn = document.getElementById('minus5');
+      this.input = document.getElementById('input5');
+    },
+    events: function () {
+      this.minusBtn.addEventListener('click', this.decrement.bind(this));
+      this.plusBtn.addEventListener('click', this.increment.bind(this));
+    },
+    decrement: function () {
+      var value = this.input.value;
+      value--;
+      this.input.value = value;  
+    },
+    increment: function () {
+      var value = this.input.value;
+      value++;
+      this.input.value = value;  
+    }
+  };
+  
+  NumberStepper.init();
+
+  var NumberStepper = {
+    init: function () {
+      this.domElems();
+      this.events();
+    },
+    domElems: function () {
+      this.plusBtn = document.getElementById('plus6');
+      this.minusBtn = document.getElementById('minus6');
+      this.input = document.getElementById('input6');
+    },
+    events: function () {
+      this.minusBtn.addEventListener('click', this.decrement.bind(this));
+      this.plusBtn.addEventListener('click', this.increment.bind(this));
+    },
+    decrement: function () {
+      var value = this.input.value;
+      value--;
+      this.input.value = value;  
+    },
+    increment: function () {
+      var value = this.input.value;
+      value++;
+      this.input.value = value;  
+    }
+  };
+  
+  NumberStepper.init();
+
+  var NumberStepper = {
+    init: function () {
+      this.domElems();
+      this.events();
+    },
+    domElems: function () {
+      this.plusBtn = document.getElementById('plus7');
+      this.minusBtn = document.getElementById('minus7');
+      this.input = document.getElementById('input7');
+    },
+    events: function () {
+      this.minusBtn.addEventListener('click', this.decrement.bind(this));
+      this.plusBtn.addEventListener('click', this.increment.bind(this));
+    },
+    decrement: function () {
+      var value = this.input.value;
+      value--;
+      this.input.value = value;  
+    },
+    increment: function () {
+      var value = this.input.value;
+      value++;
+      this.input.value = value;  
+    }
+  };
+  
+  NumberStepper.init();
+
+  var NumberStepper = {
+    init: function () {
+      this.domElems();
+      this.events();
+    },
+    domElems: function () {
+      this.plusBtn = document.getElementById('plus8');
+      this.minusBtn = document.getElementById('minus8');
+      this.input = document.getElementById('input8');
+    },
+    events: function () {
+      this.minusBtn.addEventListener('click', this.decrement.bind(this));
+      this.plusBtn.addEventListener('click', this.increment.bind(this));
+    },
+    decrement: function () {
+      var value = this.input.value;
+      value--;
+      this.input.value = value;  
+    },
+    increment: function () {
+      var value = this.input.value;
+      value++;
+      this.input.value = value;  
+    }
+  };
+  
+  NumberStepper.init();
+
+  var NumberStepper = {
+    init: function () {
+      this.domElems();
+      this.events();
+    },
+    domElems: function () {
+      this.plusBtn = document.getElementById('plus9');
+      this.minusBtn = document.getElementById('minus9');
+      this.input = document.getElementById('input9');
+    },
+    events: function () {
+      this.minusBtn.addEventListener('click', this.decrement.bind(this));
+      this.plusBtn.addEventListener('click', this.increment.bind(this));
+    },
+    decrement: function () {
+      var value = this.input.value;
+      value--;
+      this.input.value = value;  
+    },
+    increment: function () {
+      var value = this.input.value;
+      value++;
+      this.input.value = value;  
+    }
+  };
+  
+  NumberStepper.init();
+
+  var NumberStepper = {
+    init: function () {
+      this.domElems();
+      this.events();
+    },
+    domElems: function () {
+      this.plusBtn = document.getElementById('plus10');
+      this.minusBtn = document.getElementById('minus10');
+      this.input = document.getElementById('input10');
+    },
+    events: function () {
+      this.minusBtn.addEventListener('click', this.decrement.bind(this));
+      this.plusBtn.addEventListener('click', this.increment.bind(this));
+    },
+    decrement: function () {
+      var value = this.input.value;
+      value--;
+      this.input.value = value;  
+    },
+    increment: function () {
+      var value = this.input.value;
+      value++;
+      this.input.value = value;  
+    }
+  };
+  
+  NumberStepper.init();
+  
+  var NumberStepper = {
+    init: function () {
+      this.domElems();
+      this.events();
+    },
+    domElems: function () {
+      this.plusBtn = document.getElementById('plus11');
+      this.minusBtn = document.getElementById('minus11');
+      this.input = document.getElementById('input11');
+    },
+    events: function () {
+      this.minusBtn.addEventListener('click', this.decrement.bind(this));
+      this.plusBtn.addEventListener('click', this.increment.bind(this));
+    },
+    decrement: function () {
+      var value = this.input.value;
+      value--;
+      this.input.value = value;  
+    },
+    increment: function () {
+      var value = this.input.value;
+      value++;
+      this.input.value = value;  
+    }
+  };
+  
+  NumberStepper.init();
+
+  var NumberStepper = {
+    init: function () {
+      this.domElems();
+      this.events();
+    },
+    domElems: function () {
+      this.plusBtn = document.getElementById('plus12');
+      this.minusBtn = document.getElementById('minus12');
+      this.input = document.getElementById('input12');
+    },
+    events: function () {
+      this.minusBtn.addEventListener('click', this.decrement.bind(this));
+      this.plusBtn.addEventListener('click', this.increment.bind(this));
+    },
+    decrement: function () {
+      var value = this.input.value;
+      value--;
+      this.input.value = value;  
+    },
+    increment: function () {
+      var value = this.input.value;
+      value++;
+      this.input.value = value;  
+    }
+  };
+  
+  NumberStepper.init();
+
+  var NumberStepper = {
+    init: function () {
+      this.domElems();
+      this.events();
+    },
+    domElems: function () {
+      this.plusBtn = document.getElementById('plus13');
+      this.minusBtn = document.getElementById('minus13');
+      this.input = document.getElementById('input13');
+    },
+    events: function () {
+      this.minusBtn.addEventListener('click', this.decrement.bind(this));
+      this.plusBtn.addEventListener('click', this.increment.bind(this));
+    },
+    decrement: function () {
+      var value = this.input.value;
+      value--;
+      this.input.value = value;  
+    },
+    increment: function () {
+      var value = this.input.value;
+      value++;
+      this.input.value = value;  
+    }
+  };
+  
+  NumberStepper.init();
+
+  var NumberStepper = {
+    init: function () {
+      this.domElems();
+      this.events();
+    },
+    domElems: function () {
+      this.plusBtn = document.getElementById('plus14');
+      this.minusBtn = document.getElementById('minus14');
+      this.input = document.getElementById('input14');
+    },
+    events: function () {
+      this.minusBtn.addEventListener('click', this.decrement.bind(this));
+      this.plusBtn.addEventListener('click', this.increment.bind(this));
+    },
+    decrement: function () {
+      var value = this.input.value;
+      value--;
+      this.input.value = value;  
+    },
+    increment: function () {
+      var value = this.input.value;
+      value++;
+      this.input.value = value;  
+    }
+  };
+  
+  NumberStepper.init();
+
+  var NumberStepper = {
+    init: function () {
+      this.domElems();
+      this.events();
+    },
+    domElems: function () {
+      this.plusBtn = document.getElementById('plus15');
+      this.minusBtn = document.getElementById('minus15');
+      this.input = document.getElementById('input15');
+    },
+    events: function () {
+      this.minusBtn.addEventListener('click', this.decrement.bind(this));
+      this.plusBtn.addEventListener('click', this.increment.bind(this));
+    },
+    decrement: function () {
+      var value = this.input.value;
+      value--;
+      this.input.value = value;  
+    },
+    increment: function () {
+      var value = this.input.value;
+      value++;
+      this.input.value = value;  
+    }
+  };
+  
+  NumberStepper.init();
+
+  var NumberStepper = {
+    init: function () {
+      this.domElems();
+      this.events();
+    },
+    domElems: function () {
+      this.plusBtn = document.getElementById('plus16');
+      this.minusBtn = document.getElementById('minus16');
+      this.input = document.getElementById('input16');
+    },
+    events: function () {
+      this.minusBtn.addEventListener('click', this.decrement.bind(this));
+      this.plusBtn.addEventListener('click', this.increment.bind(this));
+    },
+    decrement: function () {
+      var value = this.input.value;
+      value--;
+      this.input.value = value;  
+    },
+    increment: function () {
+      var value = this.input.value;
+      value++;
+      this.input.value = value;  
+    }
+  };
+  
+  NumberStepper.init();
